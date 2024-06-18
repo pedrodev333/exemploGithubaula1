0a52d870d644c8e01e6e2ba2ee48c17a62e7cff4
pedrodev333 <pedroheriquemendonca@gmail.com>
2024-06-18 00:58:10 -0300

    Adicionado helloManyTimes.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     private static void sayHello(){
         System.out.println("Hello World!");
+    }
 
+    private static void sayHelloManyTimes(int times){
+        for (int i = 0; i<times;i++){
+            System.out.println("Hello World!");
+        }
     }
 
     public static void main(String[] args) {
         sayHello();
+        sayHelloManyTimes(10);
     }
 
 }
