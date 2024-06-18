bfc4917e79d55ed905fd3c16cdaa13d10a9e3f7d
pedrodev333 <pedroheriquemendonca@gmail.com>
2024-06-18 00:55:26 -0300

    Refatorado o main.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+    private static void sayHello(){
         System.out.println("Hello World!");
+
+    }
+
+    public static void main(String[] args) {
+        sayHello();
     }
 
 }
